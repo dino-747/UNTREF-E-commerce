3c50b0e13647daa2cc830fe8939ae2f25bb81294
Dino Mandato <shoo_dyno@hotmail.com>
2023-10-15 10:29:50 -0300

    1.0

---
+++ js/cart.js
@@ -0,0 +1,108 @@
+const productOnCart = JSON.parse(localStorage.getItem("products-On-Cart"))
+
+const carritoVacio = document.querySelector("#Carrito-vacio");
+const productsContainer = document.querySelector("#cart-products");
+const cartActions = document.querySelector("#cart-action");
+const comprado = document.querySelector("#Producto-comprado");
+let btnDelete = document.querySelectorAll(".cart-Product-Delete")
+const btnVaciar = document.querySelector("#cart-action-vaciar")
+const precioTotal = document.querySelector("#Total")
+const comprarProducto = document.querySelector("#cart-action-buy")
+
+function cargarProductosCarrito(){
+if (productOnCart && productOnCart.length > 0){
+    
+    carritoVacio.classList.add("disabled");
+    productsContainer.classList.remove("disabled");
+    cartActions.classList.remove("disabled");
+    comprado.classList.add("disabled")
+
+    productsContainer.innerHTML = "";
+
+    productOnCart.forEach(product =>{
+
+        const li = document.createElement("li");
+        li.classList.add("cart-Product");
+        li.innerHTML = `
+            <li>
+                <img class="cart-Product-img" src= "${product.imagen}" alt="${product.titulo}">
+            </li>
+            <li class="cart-Product-title">
+                <span>Titulo</span>
+                <h3>${product.titulo}</h3>
+            </li>
+            <li class="cart-Product-Cantidad">
+                <span>Cantidad</span>
+                <p>${product.cantidad}</p>
+            </li>
+            <li
+            class="cart-Product-Precio">
+                <span>Precio</span>
+                <p>$${product.precio}</p>
+            </li>
+            <li class="cart-Product-Subtotal">
+                <span>Subtotal</span>
+                <p>$${product.precio * product.cantidad}</p>
+            </li>
+            <li>
+                <span >
+                    <a href="#" id="${product.id}" class="cart-Product-Delete"><i class="fa-solid fa-trash"></i></a>
+                </span>
+            </li> `;
+
+    productsContainer.append(li)
+    })
+    btnDeleteReload()
+    actualizarTotal()
+
+}else {
+    carritoVacio.classList.remove("disabled");
+    productsContainer.classList.add("disabled");
+    cartActions.classList.add("disabled");
+    comprado.classList.add("disabled");
+}
+    
+}
+cargarProductosCarrito()
+
+function btnDeleteReload(){
+    btnDelete = document.querySelectorAll(".cart-Product-Delete")
+
+    btnDelete.forEach(boton =>{
+        boton.addEventListener("click", deleteProduct)
+    });
+}
+
+function deleteProduct(e){
+    const idBtn = e.currentTarget.id;
+    const index = productOnCart.findIndex(producto => producto.id === idBtn);
+    productOnCart.splice(index, 1);
+    cargarProductosCarrito();
+    //btnDeleteReload()
+    
+    localStorage.setItem("products-On-Cart", JSON.stringify(productOnCart));
+}
+
+btnVaciar.addEventListener("click", vaciarCarrito)
+function vaciarCarrito(){
+    productOnCart.length = 0;
+    localStorage.setItem("products-On-Cart", JSON.stringify(productOnCart));
+    cargarProductosCarrito();
+}
+
+function actualizarTotal(){
+    const calcularTotal = productOnCart.reduce((count, producto) => count + (producto.precio * producto.cantidad), 0);
+    Total.innerText =`$${calcularTotal}`;
+}
+
+comprarProducto.addEventListener("click", comprarTodo);
+function comprarTodo(){
+
+    productOnCart.length = 0;
+    localStorage.setItem("products-On-Cart", JSON.stringify(productOnCart));
+
+    carritoVacio.classList.add("disabled");
+    productsContainer.classList.add("disabled");
+    cartActions.classList.add("disabled");
+    comprado.classList.remove("disabled");
+}
---
+++ js/main.js
@@ -0,0 +1,92 @@
+let products= [];
+
+fetch("js/products.json")
+    .then(Response => Response.json())
+    .then(function(data) {
+        products = data;
+        uploadProducts(products)
+    })
+
+//llamo a los elementos de DOM que voy a usar
+const productContainer = document.querySelector("#product-container");
+const search = document.querySelector(".search")
+const filterOptions = document.querySelectorAll(".option")
+let btnAdd = document.querySelectorAll(".add")
+const counter = document.querySelector("#count")
+
+
+const uploadProducts = function(allProducts){
+    productContainer.innerHTML= "";
+    
+    allProducts.forEach(productos => { //por cada elemento de array
+
+        const li = document.createElement("li"); //creo el li que va a ser mi contenedor del producto
+        li.classList.add("product"); //le doy una clase
+        li.innerHTML = ` 
+            <h3 class="titulo">${productos.titulo}</h3>
+            <img class="imagen" src="${productos.imagen}" alt="${productos.titulo}">
+            <p class="precio">$${productos.precio}</p>
+            <span class="add" id=${productos.id}><a  href="#">Agregar <i class="fa-solid fa-cart-plus fa-xs"></i></a></span>
+        `; //le digo todo lo que quiero que contenga linkeando datos del array
+
+        productContainer.append(li);
+        
+    })
+    btnReload();
+};
+
+//uploadProducts(products)
+
+filterOptions.forEach( boton =>{
+    boton.addEventListener("click", (e) =>{
+    if (e.currentTarget.id != "Reset"){
+        const marca = products.filter(productos => productos.categoria.marca === e.currentTarget.id || productos.categoria.ram === e.currentTarget.id || productos.categoria.memoria === e.currentTarget.id)
+        uploadProducts(marca);
+    }else if(e.currentTarget.id === "Reset"){
+            uploadProducts(products);
+        }
+    })
+})
+//como mis botones son de una etiqueta "a" con esta funcion evito que se recarge y pierda los datos cada vez que hago click o le aplico algun filtro
+function btnReload(){
+    const btnAdd = document.querySelectorAll(".add")
+
+    btnAdd.forEach(boton =>{
+        boton.addEventListener("click", addCart)
+    });
+}
+let productOnCart;
+
+let productOnCartLS = localStorage.getItem("products-On-Cart");
+if(productOnCartLS){
+    productOnCart = JSON.parse(productOnCartLS);
+    refreshCount();
+}else {
+    productOnCart = [];
+}
+ //creo el array del carrito
+//const idBtn selecciona el id del boton
+//productAdd busca el id del producto dentro del array que coincida con el id del boton
+function addCart(e){
+    const idBtn = e.currentTarget.id; 
+    const productAdd = products.find(product => product.id === idBtn);
+    //le digo que busque en el carrito con la propiedad some si existe un producto como el que intento agregar, despues el indico con findIndex que me busque la posicion del id "agregado" en el array y a si hay una coincidencia le agrege 1 a la cantidad 
+    if(productOnCart.some(producto => producto.id === idBtn)) {
+        const index = productOnCart.findIndex(producto => producto.id === idBtn);
+        productOnCart[index].cantidad++;
+    } else {
+        productAdd.cantidad = 1;
+        productOnCart.push(productAdd);
+    } 
+    refreshCount();
+
+    localStorage.setItem("products-On-Cart", JSON.stringify(productOnCart));
+}//guardo en el localStorage la informacion del carrito para que no se pierda y poder usarla en otro html
+
+//le asigno un nuevo elemento al array para contar la cantidad del mismo producto que agrego al carrito
+    
+// esta funcion me sirbe para llevar la cuenta de la cantidad de productos que tengo en el carrito, entonces le digo con la propiedad reduce que al valor anterior(count) le sume la cantidad del producto(product) y que el numero inicial sea cero.. se la agrego debajo de la funcion addCart para que se valla actualizando cada vez que agrego algun producto
+function refreshCount(){ 
+    let Number = productOnCart.reduce((count, product) => count + product.cantidad, 0);
+counter.innerText = Number;
+} 
